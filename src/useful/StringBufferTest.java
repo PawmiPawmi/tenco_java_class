e8f4e858dc94a040003b20861da3ee982ae42dbb
esh <z9901z@naver.com>
2024-04-29 09:29:14 +0900

    2024.4.29

--- src/useful/StringBufferTest.java
+++ src/useful/StringBufferTest.java
@@ -6,7 +6,7 @@ public class StringBufferTest {
 	// 코드의 시작점 - (메인 작업자)
 	public static void main(String[] args) {
 
-		String str1 = new String("Hello");
+		String str1 = "Hello";
 		String str2 = new String("World!");
 		// 힙 메모리에 객체 2개 생성
 
@@ -23,6 +23,7 @@ public class StringBufferTest {
 		// 새로운 메모리를 할당한 것이 아닌 변경한 것이다.
 		
 		String newStr=bufferStr.toString();
+		System.out.println(bufferStr);
 		
 
 	} // end of main
